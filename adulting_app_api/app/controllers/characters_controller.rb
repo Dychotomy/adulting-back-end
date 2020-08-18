999b3928a979fcd4f41518e1e0346689616404ac
dychotomy <dychotomy2000@yahoo.com>
2020-08-18 18:02:51 -0400

    Removed non essential routes

--- adulting_app_api/app/controllers/characters_controller.rb
+++ adulting_app_api/app/controllers/characters_controller.rb
@@ -13,30 +13,31 @@ class CharactersController < ApplicationController
     render json: @character
   end
 
+  # Left it for post MVP creating your own characters.
   # POST /characters
-  def create
-    @character = Character.new(character_params)
+  # def create
+  #   @character = Character.new(character_params)
 
-    if @character.save
-      render json: @character, status: :created, location: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @character.save
+  #     render json: @character, status: :created, location: @character
+  #   else
+  #     render json: @character.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PATCH/PUT /characters/1
-  def update
-    if @character.update(character_params)
-      render json: @character
-    else
-      render json: @character.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /characters/1
+  # def update
+  #   if @character.update(character_params)
+  #     render json: @character
+  #   else
+  #     render json: @character.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /characters/1
-  def destroy
-    @character.destroy
-  end
+  # # DELETE /characters/1
+  # def destroy
+  #   @character.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -44,8 +45,8 @@ class CharactersController < ApplicationController
       @character = Character.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def character_params
-      params.require(:character).permit(:name, :description, :physical, :social, :mental, :resources, :artwork)
-    end
+    # # Only allow a trusted parameter "white list" through.
+    # def character_params
+    #   params.require(:character).permit(:name, :description, :physical, :social, :mental, :resources, :artwork)
+    # end
 end
--- adulting_app_api/app/controllers/events_controller.rb
+++ adulting_app_api/app/controllers/events_controller.rb
@@ -13,39 +13,11 @@ class EventsController < ApplicationController
     render json: @event.to_json(include: :event_result)
   end
 
-  # POST /events
-  def create
-    @event = Event.new(event_params)
-
-    if @event.save
-      render json: @event, status: :created, location: @event
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /events/1
-  def update
-    if @event.update(event_params)
-      render json: @event
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /events/1
-  def destroy
-    @event.destroy
-  end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def event_params
-      params.require(:event).permit(:description, :option_a, :option_b, :artwork)
-    end
 end
